b6b503f22a0396167376424d2523bbfcee27959e
KhoomeiK <rohan.pandey@gmail.com>
2020-05-06 09:18:09 -0700

    ucUsers

--- users.py
+++ users.py
@@ -4,10 +4,10 @@ import time
 from os import listdir
 
 # use users who post on sad subreddits' text to classify as depressed
-subs, users = set(), set()
+subs = set()
 
 # subs.add('depression')
-with open('subreddits.txt', 'r') as txt:
+with open('src/ucSubs.txt', 'r') as txt:
     for line in txt:
         if line[0] != '*' and 'r/' in line:
             sub = line.split('r/')[1].strip()
@@ -19,57 +19,58 @@ r = praw.Reddit(username = "WebsterBot",
     client_id = "_fRmFVzOM5jYHQ",
     client_secret = "o_jPN2Mou9UbAJzN5I3zybCaKjo",
     user_agent = "actualsnek wtwbot test 0.0")
+'''
+for sub in subs:
+    users = set()
+    print('SUB:', sub)
+    writer = csv.writer(open('src/ucUsers.csv', 'a'))
+    for p in r.subreddit(sub).top(time_filter='month'): # CHANGE: year?
+        if p.score > 100 and p.author:
+            print(p.author)
+            users.add(p.author)
+            for c in p.comments:
+                if isinstance(c, praw.models.Comment) and c.author and c.score > 0:
+                    users.add(c.author)
+                    if len(c.replies) > 0:
+                        for child in c.replies.list():
+                            if isinstance(child, praw.models.Comment) and child.author and child.score > 0:
+                                users.add(child.author)
+    for user in users:
+        writer.writerow([user.name])
+    print('LEN:', len(users)) # users who've commented on sub in past day
+
+'''
+users = set()
+with open('src/ucUsers.csv', 'r') as userList:
+	for u in userList:
+		users.add(praw.models.Redditor(reddit=r, name=u.strip()))
+print('TOTAL LEN:', len(users))
 
 def commentData(c):
     return (c.subreddit.display_name, c.id, c.parent_id, c.created_utc, c.body)
 
-# for sub in subs:
-#     print(sub)
-#     writer = csv.writer(open('./userList.csv', 'w'))
-#     for user in users:
-#     	writer.writerow(user.name)
-#     for p in r.subreddit(sub).top(time_filter='month'): # CHANGE: year?
-#         if p.score > 100 and p.author:
-#             users.add(p.author)
-#             for c in p.comments:
-#                 if isinstance(c, praw.models.Comment) and c.author and c.score > 0:
-#                     users.add(c.author)
-#                     if len(c.replies) > 0:
-#                         for child in c.replies.list():
-#                             if isinstance(child, praw.models.Comment) and child.author and child.score > 0:
-#                                 users.add(child.author)
-    # print(users) # users who've commented on sub in past day
-
-# users = [praw.models.Redditor(reddit=r, name='imhereforthepuppies')] # CHANGE: remove
-with open('./userList.csv', 'r') as userList:
-	for u in userList:
-		users.add(praw.models.Redditor(reddit=r, name=u.replace(',', '').strip() ))
-
-print(users)
+worked, errored = 0, 0
 
 for user in users:
-    if '%s.csv' % user.name not in listdir('./users'):
+    if '%s.csv' % user.name not in listdir('src/ucUsers'):
         print(user)
-        count = {}
+        # count = {}
         saved = []
         comments = user.comments.new()
         try:
-	        for comment in comments:
-	            # print(comment.subreddit.display_name, comment.created_utc)
-	            saved.append(comment)
-	            if time.time() - comment.created_utc > 2592000: # past 30 days
-	                break
-	            if comment.subreddit.display_name in subs:
-	                if comment.subreddit.display_name in count:
-	                    count[comment.subreddit.display_name] = count[comment.subreddit.display_name] + 1  
-	                else: 
-	                    count[comment.subreddit.display_name] = 1
-        	# print(count)
-	        if any(True if ct >= 3 else False for ct in count.values()):
-	            writer = csv.writer(open('./users/%s.csv' % user.name, 'w'))
-	            writer.writerow(('subreddit', 'self ID', 'parent ID', 'time', 'text'))
-	            for comment in saved:
-	                # print(commentData(comment))
-	                writer.writerow(commentData(comment))
+            worked += 1
+            for comment in comments:
+                saved.append(comment)
+                if time.time() - comment.created_utc > 2592000: # past 30 days
+                    break
+            writer = csv.writer(open('src/ucUsers/%s.csv' % user.name, 'w'))
+            writer.writerow(('subreddit', 'self ID', 'parent ID', 'time', 'text'))
+            for comment in saved:
+                # print(commentData(comment))
+                writer.writerow(commentData(comment))
         except:
+        	errored += 1
         	continue
+
+print(worked, errored, worked + errored)
+#'''
